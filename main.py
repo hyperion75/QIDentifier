c304b3736452f8853180b10cc82aacaa4a91e191
Matthew Eddy <meddy@qualys.com>
2021-08-30 23:23:51 -0400

    Completed revamped settings UI, began main UI

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
+import tkinter.font as tkFont
+from tkinter import ttk
 import PullSigs
 import KBPull
 import vspull
 from tkinter import *
 import base64
 import keyring
-from tkinter import ttk
 
 # create root window
 root = Tk()
 root.eval('tk::PlaceWindow . center')
 root.title("QIDentifier")
 root.geometry('1280x720')
+root.tk.call("source", "sun-valley.tcl")
+root.tk.call("set_theme", "light")
 
-#Required for checkbox functionality
+# Required for checkbox functionality
 exclude_kb_on = BooleanVar()
+
 
 def closewindow(x):
     x.destroy()
+
 
 def set_vs_version(ver):
     version = ver.get()
     keyring.set_password("QIDentifier.VS_VER", "VS", version)
     vsTitle.config(text="VulnSigs Sandbox: " + version)
 
-#Started work on a button to switch between QID/CVE search methods
+
+# Started work on a button to switch between QID/CVE search methods
 """def searchmethod(x):
     global sm
     if x == 0:
@@ -34,7 +40,8 @@ def set_vs_version(ver):
         switchbuttonx.config(text="QID:")
         sm = 0
         print("Searchmethod set to " + str(sm))
-    #searchmethod = Button(topframe, text="QID:", bg='#FFFFFF', font=arialheader, command=searchmethod())"""
+    #searchmethod = ttk.Button(topframe, text="QID:", bg='#FFFFFF', font=arialheader, command=searchmethod())"""
+
 
 # 2 functions - Set the POD in keychain, then set the API login string in keychain.
 
@@ -55,6 +62,7 @@ def storecredentials(username, password, pod):
     keyring.set_password("QIDentifier.USER", "QIDer", username)
     keyring.set_password("QIDentifier.PASS", "QIDer", password)
 
+
 # Changed behavior of User/Pass/POD variable storage for added flexibility with new features.
 # Encoding functionality moved to KBPull.py.
 """def buildbase64(username, password, pod):
@@ -74,109 +82,107 @@ def storecredentials(username, password, pod):
     base64_encode = base64_bytes.decode('ascii')
     keyring.set_password("QIDentifier", "API", base64_encode)"""
 
-#Popup window for POD Selection / API Login
-def loginpopup():
-    loginwindow = Toplevel(root)
-    root.eval(f'tk::PlaceWindow {str(loginwindow)} center')
-    loginwindow.title("Qualys API Credentials")
-    loginwindow.geometry("400x250")
 
-    instruction_label = Label(loginwindow, text="Please select a POD and enter your API credentials:")
-    instruction_label.pack()
+# Popup window for POD Selection / API Login
+def settingspane():
+    settings = Toplevel(root)
+    root.eval(f'tk::PlaceWindow {str(settings)} center')
+    settings.title("QIDentifier Settings")
+    settings.geometry("750x285")
+    settings.resizable(False, False)
 
-    pod_label = Label(loginwindow, text="POD:")
-    pod_label.pack()
+    # settings UI positioning
+    credentialframe = ttk.LabelFrame(settings, text="API Credentials", padding=(20,10))
+    credentialframe.grid(row=1, column=1, padx=(20,10), pady=(20,10), sticky="nsew", rowspan=2)
+    vulnsigsframe = ttk.LabelFrame(settings, text="VULNSIGS Settings", padding=(20,10))
+    vulnsigsframe.grid(row=1, column=2, padx=(20,10), pady=(20,10), sticky="n")
+
+    # settings UI elements
+    closebutton = ttk.Button(settings, text="Done", width=10, command=lambda: closewindow(settings))
+    closebutton.grid(row=2,column=2, padx=(20,10), pady=(20,10), sticky="n")
+
+    # login settings
+    pod_label = ttk.Label(credentialframe, text="POD:")
+    pod_label.grid(row=0, column=0, pady=15)
     pod = StringVar()
-    pod_entry = ttk.Combobox(loginwindow, width = 27, textvariable = pod)
+    pod_entry = ttk.Combobox(credentialframe, width=15, textvariable=pod)
     pod_entry['values'] = (' US POD 1',
                            ' US POD 2',
                            ' US POD 3')
-    pod_entry.pack()
+    pod_entry.grid(row=0, column=1)
 
-    username_label = Label(loginwindow, text="Username:")
-    username_label.pack()
+    username_label = ttk.Label(credentialframe, text="Username:")
+    username_label.grid(row=1, column=0, pady=15)
     username = StringVar()
-    username_entry = Entry(loginwindow, textvariable=username)
-    username_entry.pack()
+    username_entry = ttk.Entry(credentialframe, width=15, textvariable=username)
+    username_entry.grid(row=1, column=1)
 
-    password_label = Label(loginwindow, text="Password:")
-    password_label.pack()
+    password_label = ttk.Label(credentialframe, text="Password:")
+    password_label.grid(row=2, column=0, pady=15)
     password = StringVar()
-    password_entry = Entry(loginwindow, show='*', textvariable=password)
-    password_entry.pack()
+    password_entry = ttk.Entry(credentialframe, show='*', width=15, textvariable=password)
+    password_entry.grid(row=2, column=1)
 
-    login_confirm = Button(loginwindow, text="Set Credentials", width=12, height=1,
-                           command=lambda : storecredentials(username, password, pod))
-    login_confirm.pack()
-    okbutton = Button(loginwindow, text="OK", width=10, height=1, command=lambda : closewindow(loginwindow))
-    okbutton.pack()
+    login_confirm = ttk.Button(credentialframe, text="Set Credentials", width=12,
+                               command=lambda: storecredentials(username, password, pod))
+    login_confirm.grid(row=3, column=1, pady=15)
 
-#Popup window for VULNSIGS version selection
-def vsupdatepopup():
-    vsuwindow = Toplevel(root)
-    root.eval(f'tk::PlaceWindow {str(vsuwindow)} center')
-    vsuwindow.title("VulnSigs Version Selector")
-    vsuwindow.geometry("250x100")
-
-    instruction_label = Label(vsuwindow, text="Select a VulnSigs Version:")
-    instruction_label.pack()
+    instruction_label_vs = ttk.Label(vulnsigsframe, text="VULNSIGS Version:")
+    instruction_label_vs.grid(row=0, column=0, pady=15, padx=10)
 
     versiontuple = tuple(vspull.vspull())
     ver = StringVar()
-    ver_entry = ttk.Combobox(vsuwindow, width = 27, textvariable = ver)
+    ver_entry = ttk.Combobox(vulnsigsframe, width=20, textvariable=ver)
     ver_entry['values'] = versiontuple
-    ver_entry.pack()
+    ver_entry.grid(row=0, column=1)
 
-    vs_confirm = Button(vsuwindow, text="Set Version", width=12, height=1, command=lambda : set_vs_version(ver))
-    vs_confirm.pack()
+    vs_confirm = ttk.Button(vulnsigsframe, text="Set Version", width=12, command=lambda: set_vs_version(ver))
+    vs_confirm.grid(row=2, column=1, pady=15)
 
-    okbutton = Button(vsuwindow, text="OK", width=10, height=1, command=lambda : closewindow(vsuwindow))
-    okbutton.pack()
-
-#Executed when user clicks the "retrieve information" button. This does all the work.
+# Executed when user clicks the "retrieve information" button. This does all the work.
 def pullinfo():
     qid = qidInput.get()
-    vs_output.delete(1.0,END)
-    kb_output.delete(1.0,END)
+    vs_output.delete(1.0, END)
+    kb_output.delete(1.0, END)
     # Display Sandbox Information
     vs_display = PullSigs.pullsigs(qid)
     # Display KB Information (if checkbox unchecked)
-    if exclude_kb_on.get()==0:
+    if exclude_kb_on.get() == 0:
         kb_display = KBPull.pullkb(qid)
         kb_output.insert(END, kb_display)
     vs_output.insert(END, vs_display)
 
-#Everything below this is UI position related
-#Top, Middle, Bottom, Footer frame definitions
+
+# Everything below this is UI position related
+# Top, Middle, Bottom, Footer frame definitions
 frame = Frame(root)
 frame.pack()
-topframe = Frame(root, relief=RAISED, borderwidth=2)
-topframe.pack(side=TOP, fill='x')
+topframe = Frame(root)
+topframe.pack(side=TOP, fill='x', pady=10)
 midframe = Frame(root)
-midframe.pack(side=TOP, fill='x', ipadx=30)
+midframe.pack(side=TOP, fill='x', pady=2)
 footerframe = Frame(root)
 footerframe.pack(side=BOTTOM, fill='x')
 bottomframe = Frame(root)
 bottomframe.pack(side=BOTTOM, expand=True, fill=BOTH)
 
-#fonts
+# fonts
 arialheader = ("Arial", 16, "bold")
 arialbold = ("Arial", 12, "bold")
 arial = ("Arial", 12)
 
-#UI Elements
-#sm = 0
-switchbuttonx = Label(topframe, text="QID:", bg='#FFFFFF', font=arialheader)
-switchbuttonx.pack(side=LEFT)
+# UI Elements
+# sm = 0
+switchbuttonx = ttk.Label(topframe, text="QID:", font=arialheader)
+switchbuttonx.pack(side=LEFT, padx=5)
 
-qidInput = Entry(topframe, width=12)
-qidInput.pack(side=LEFT)
+qidInput = ttk.Entry(topframe, width=12)
+qidInput.pack(side=LEFT, padx=5)
 
-btn_retrieve = Button(topframe, text="Go",
-             fg="orange red", command=pullinfo, font=arial)
+btn_retrieve = ttk.Button(topframe, text="Go", command=pullinfo)
 btn_retrieve.pack(side=LEFT)
 
-exclude_kb = Checkbutton(topframe, text="Exclude KB Info", font=arial, variable=exclude_kb_on)
+exclude_kb = ttk.Checkbutton(topframe, text="Exclude KB Info", variable=exclude_kb_on, style="Switch.TCheckbutton")
 exclude_kb.pack(side=RIGHT)
 
 # vsTitle is set to global so that it's updated automatically when you change VULNSIGS version.
@@ -186,25 +192,36 @@ if keyring.get_password("QIDentifier.VS_VER", "VS") == None:
 elif keyring.get_password("QIDentifier.VS_VER", "VS") != "":
     vsText = "VulnSigs Sandbox: " + keyring.get_password("QIDentifier.VS_VER", "VS")
 
-vsTitle = Label(midframe, text=vsText, font=arialbold)
-vsTitle.pack(side=LEFT)
+vsTitle = ttk.Label(midframe, text=vsText, font=arialbold)
+vsTitle.pack(side=LEFT, padx=5)
 
-kbTitle = Label(midframe, text="Qualys Knowledgebase", font=arialbold)
-kbTitle.pack(side=RIGHT)
+kbTitle = ttk.Label(midframe, text="Qualys Knowledgebase", font=arialbold)
+kbTitle.pack(side=RIGHT, padx=5)
 
-vs_output = Text(bottomframe, bg="alice blue", font=arial)
+#define the VS Signatures Notebook
+leftbook = ttk.Notebook(bottomframe)
+leftbook.pack(side=LEFT, fill=BOTH, expand=True)
+
+#VULNSIGS notebook, signatures page
+lb_tab1 = ttk.Frame(leftbook)
+for index in [0, 1]:
+    lb_tab1.columnconfigure(index=index, weight=1)
+    lb_tab1.rowconfigure(index=index, weight=1)
+leftbook.add(lb_tab1, text="Signatures")
+
+vs_output = Text(lb_tab1, font=arial)
 vs_output.pack(side=LEFT, expand=True, fill=BOTH)
+
+#VULNSIGS notebook, functions page
+lb_tab2 = ttk.Frame(leftbook)
+leftbook.add(lb_tab2, text="Functions")
 
 kb_output = Text(bottomframe, bg="gainsboro", font=arial)
 kb_output.pack(side=RIGHT, expand=True, fill=BOTH)
 kb_output.insert(END, "Pulling KB information may take some time.\n\nIf you don't need it, check the box above.")
 
-config_acct = Button(footerframe, text="Configure API Account", command=lambda : loginpopup())
-config_acct.pack(side=RIGHT)
-
-config_vs_ver = Button(footerframe, text="Set VULNSIGS Version", command=lambda : vsupdatepopup())
-config_vs_ver.pack(side=LEFT)
+settings = ttk.Button(footerframe, text="Settings", command=lambda: settingspane())
+settings.pack(side=RIGHT)
 
 # Execute Tkinter
 root.mainloop()
-
